a05023cbf79504ac8c5295071ff9d486fcf6382d
Alexey Ustinnikov <a.ustinnikov@grfc.ru>
2024-03-09 16:10:42 +0300

    First test passed ok.

--- Database.php
+++ Database.php
@@ -9,13 +9,28 @@ class Database implements DatabaseInterface
 {
     private mysqli $mysqli;
 
+    private function __fill_db()
+    {
+        echo "Try to fill the db" . PHP_EOL;
+        $this->mysqli->query("CREATE TABLE IF NOT EXISTS users (
+            user_id int primary key NOT NULL AUTO_INCREMENT,
+            name text NOT NULL,
+            block int(11) NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;");
+
+    }
     public function __construct(mysqli $mysqli)
     {
         $this->mysqli = $mysqli;
+        $this->__fill_db();
     }
+
 
     public function buildQuery(string $query, array $args = []): string
     {
+        if (!$args) {
+            return $query;
+        }
         throw new Exception();
     }
 
